2b11ab82c150dd2e7eeb175c535de2a41f28fe7e
RandomGuy0114 <clarenceperez686@gmail.com>
2022-12-15 16:49:58 +0800

    footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,23 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 
 
 function Home() {
-  const {post, setPosts} = useState({});
+  // const {post, setPosts} = useState({});
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-      setPosts(data.slice(0, 10));
-     set
-    });
-  }, []);
-  return (
-    <div>
-      <h1>This is the home page</h1>
-      <h2> THis is a test</h2>
-    </div>
-  );
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     setPosts(data.slice(0, 10));
+  //    set
+  //   });
+  // }, []);
+  // return (
+  //   <div>
+  //     <h1>This is the home page</h1>
+  //     <h2> THis is a test</h2>
+  //   </div>
+  // );
 }
   
   export default Home;
