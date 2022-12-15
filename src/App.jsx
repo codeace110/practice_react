bfaef04f31ce0afb214c16aee32f7e80e8044a5c
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2022-12-15 16:55:34 +0800

    API body done

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,13 @@ import NavbarFooter from "./NavbarFooter"
 
 function App() {
     return (
-
-            
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="about" element={<About />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="*" element={<NavbarFooter/>} />
-                <Route path="*" element={<Notfound/>} />
-            </Routes>
-      
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NavbarFooter />} />
+            <Route path="*" element={<Notfound />} />
+        </Routes>
     )
 }
 
