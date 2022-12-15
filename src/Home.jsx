442bdacca731af8a9fb3326c536d67fe5ded2541
PJAngeles023 <angeles.patrickjerowin@yahoo.com>
2022-12-15 16:35:59 +0800

    API fetch

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,17 @@
-
+import {useState, useEffect} from 'react';
 
 
 function Home() {
+  const {post, setPosts} = useState({});
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+      setPosts(data.slice(0, 10));
+     set
+    });
+  }, []);
   return (
     <div>
       <h1>This is the home page</h1>
